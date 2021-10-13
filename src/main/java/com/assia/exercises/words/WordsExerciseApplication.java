db6825712f763a7c929456e807ac00454db83274
Cesar Delgado <cdelgado@assia-inc.com>
2021-10-13 11:12:12 +0200

    Adding Spring Boot Skeleton

---
+++ src/main/java/com/assia/exercises/words/WordsExerciseApplication.java
@@ -0,0 +1,13 @@
+package com.assia.exercises.words;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WordsExerciseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WordsExerciseApplication.class, args);
+	}
+
+}
